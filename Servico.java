7e8e9a605cbd37dabee4e9bc3821b6c570364fbb
gustavogabrielgomes12345-sketch <gustavogabrielgomes12345@gmail.com>
2026-08-11 22:15:23 -0300

    Delete Servico.java

--- Servico.java
+++
@@ -1,7 +0,0 @@
-public class Servico {
-    public int codigo;    
-    public String nomeServico;
-    public String descricao;
-    public int tempoEstimadoMinutos;
-    public double valorMaoObra;
-}
